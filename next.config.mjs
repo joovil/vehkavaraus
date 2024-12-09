2459aaf228c98fcb50ea426b7ac2607212b37c41
JoonasMV <JoonasMRViljanen@gmail.com>
2024-12-09 10:47:56 +0200

    Refactor next.config.mjs and add logging, update .gitignore

--- next.config.mjs
+++ next.config.mjs
@@ -9,28 +9,8 @@ export default (phase) => {
    * @type {import('next').NextConfig}
    */
 
-  if (phase === PHASE_DEVELOPMENT_SERVER && phase === PHASE_PRODUCTION_BUILD) {
-    const requiredEnvVars = [
-      "DB_NAME",
-      "DB_HOST",
-      "DB_USER",
-      "DB_PASSWORD",
-      "DB_PORT",
-      "TOKEN_SECRET",
-      "NEXT_PUBLIC_API_URL",
-      "NEXTAUTH_URL",
-      "NEXTAUTH_SECRET",
-    ];
-
-    requiredEnvVars.forEach((envVar) => {
-      if (!process.env[envVar]) {
-        throw new Error(`\x1b[31m${envVar} is missing\x1b[0m`);
-      }
-    });
-
-    if (isNaN(Number(process.env.DB_PORT))) {
-      throw new Error(`\x1b[31mDB_PORT must be a number\x1b[0m`);
-    }
+  if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
+    checkEnvVars();
   }
 
   const nextConfig = {
@@ -42,7 +22,34 @@ export default (phase) => {
         },
       ],
     },
+    logging: {
+      fetches: {
+        hmrRefreshes: true,
+        fullUrl: true,
+      },
+    },
   };
 
   return nextConfig;
 };
+
+const checkEnvVars = () => {
+  requiredEnvVars.forEach((envVar) => {
+    if (!process.env[envVar]) {
+      throw new Error(`\x1b[31m${envVar} is missing\x1b[0m`);
+    }
+  });
+};
+
+const requiredEnvVars = [
+  "DB_NAME",
+  "DB_HOST",
+  "DB_USER",
+  "DB_PASSWORD",
+  "DB_PORT",
+  "TOKEN_SECRET",
+  "NEXT_PUBLIC_API_URL",
+  "NEXTAUTH_URL",
+  "NEXTAUTH_SECRET",
+  "RESEND_API_KEY",
+];
