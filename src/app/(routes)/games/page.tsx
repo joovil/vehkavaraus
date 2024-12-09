1eec7e9a422114987558385baf7cce0df4b0330b
JoonasMV <JoonasMRViljanen@gmail.com>
2024-12-09 10:49:28 +0200

    Replace server action with fetch

--- src/app/(routes)/games/page.tsx
+++ src/app/(routes)/games/page.tsx
@@ -1,9 +1,9 @@
 import ImageBasic from "@/components/ImageBasic";
-import { getAllGames } from "@/database/repositories/gameRepository";
+import { getGamesService } from "@/lib/services/games/getGamesService";
 import BorrowGameButtons from "./components/BorrowGameButtons";
 
 const Games = async () => {
-  const games = await getAllGames();
+  const games = await getGamesService();
 
   return (
     <div className="w-1/2 gap-y-8 m-auto flex flex-col grid-cols-[repeat(auto-fit,minmax(300px,auto))] items-center min-[1250px]:justify-between min-[1250px]:grid">
