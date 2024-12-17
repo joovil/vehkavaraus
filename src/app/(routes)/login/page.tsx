823e652d6a863d724505c7782f103cf8c3665255
JoonasMV <JoonasMRViljanen@gmail.com>
2024-12-17 08:40:20 +0200

    changed css classname

--- src/app/(routes)/login/page.tsx
+++ src/app/(routes)/login/page.tsx
@@ -13,9 +13,9 @@ export default function Home() {
   };
 
   return (
-    <div className="w-fit absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-2xl">
-      <div className="bg-offWhiteV flex-col h-fit p-5 shadow-primary">
-        <h1 className="font-bold mb-3 text-center text-3xl">Log in</h1>
+    <div className="absolute left-1/2 top-1/2 w-fit -translate-x-1/2 -translate-y-1/2 text-2xl">
+      <div className="box-basic">
+        <h1 className="mb-3 text-center text-3xl font-bold">Log in</h1>
 
         <form className="flex flex-col" onSubmit={handleLogin}>
           <label>Username</label>
@@ -36,7 +36,7 @@ export default function Home() {
             placeholder="Password"
           ></input>
 
-          <div className="flex w-full justify-center mt-3">
+          <div className="mt-3 flex w-full justify-center">
             <button className="btn-primary">Log in</button>
           </div>
         </form>
