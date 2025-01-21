5fd12301633e371a54bd844293b7b05ab3657752
JoonasMV <JoonasMRViljanen@gmail.com>
2025-01-21 22:03:36 +0200

    Separate db connections for dev and prod

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,23 +1,37 @@
+import {
+  DB_HOST,
+  DB_NAME,
+  DB_PASSWORD,
+  DB_PORT,
+  DB_USER,
+} from "@/lib/utils/envVariables";
 import { Database } from "@/types";
-import { CamelCasePlugin, Kysely } from "kysely";
+import { CamelCasePlugin, Kysely, PostgresDialect } from "kysely";
 import { NeonDialect } from "kysely-neon";
+import { Pool } from "pg";
 
-// const dialect = new PostgresDialect({
-//   pool: new Pool({
-//     database: DB_NAME,
-//     host: DB_HOST,
-//     user: DB_USER,
-//     password: DB_PASSWORD,
-//     port: DB_PORT,
-//     max: 10,
-//   }),
-// });
+let db: Kysely<Database>;
 
-const db = new Kysely<Database>({
-  dialect: new NeonDialect({
-    connectionString: process.env.DATABASE_URL,
-  }),
-  plugins: [new CamelCasePlugin()],
-});
+if (process.env.NODE_ENV == "production") {
+  db = new Kysely<Database>({
+    dialect: new NeonDialect({
+      connectionString: process.env.DATABASE_URL,
+    }),
+    plugins: [new CamelCasePlugin()],
+  });
+} else {
+  db = new Kysely<Database>({
+    dialect: new PostgresDialect({
+      pool: new Pool({
+        database: DB_NAME,
+        host: DB_HOST,
+        user: DB_USER,
+        password: DB_PASSWORD,
+        port: DB_PORT,
+      }),
+    }),
+    plugins: [new CamelCasePlugin()],
+  });
+}
 
 export default db;
