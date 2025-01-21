6418d62e07ea278b70eae97c33b611ea6615611a
JoonasMV <JoonasMRViljanen@gmail.com>
2025-01-21 11:03:57 +0200

    fixed server error

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,40 +1,23 @@
-import {
-  DB_HOST,
-  DB_NAME,
-  DB_PASSWORD,
-  DB_PORT,
-  DB_USER,
-} from "@/lib/utils/envVariables";
 import { Database } from "@/types";
-import { CamelCasePlugin, Kysely, PostgresDialect } from "kysely";
+import { CamelCasePlugin, Kysely } from "kysely";
 import { NeonDialect } from "kysely-neon";
-import { Pool } from "pg";
 
-const dialect = new PostgresDialect({
-  pool: new Pool({
-    database: DB_NAME,
-    host: DB_HOST,
-    user: DB_USER,
-    password: DB_PASSWORD,
-    port: DB_PORT,
-    max: 10,
+// const dialect = new PostgresDialect({
+//   pool: new Pool({
+//     database: DB_NAME,
+//     host: DB_HOST,
+//     user: DB_USER,
+//     password: DB_PASSWORD,
+//     port: DB_PORT,
+//     max: 10,
+//   }),
+// });
+
+const db = new Kysely<Database>({
+  dialect: new NeonDialect({
+    connectionString: process.env.DATABASE_URL,
   }),
+  plugins: [new CamelCasePlugin()],
 });
-
-let db: Kysely<Database>;
-
-if (process.env.NODE_ENV == "production") {
-  db = new Kysely<Database>({
-    dialect: new NeonDialect({
-      connectionString: process.env.DATABASE_URL,
-    }),
-    plugins: [new CamelCasePlugin()],
-  });
-} else {
-  db = new Kysely<Database>({
-    dialect,
-    plugins: [new CamelCasePlugin()],
-  });
-}
 
 export default db;
