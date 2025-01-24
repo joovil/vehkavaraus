6c3839422c65f1638a2bbca4bb0feba961245531
JoonasMV <JoonasMRViljanen@gmail.com>
2025-01-24 11:57:05 +0200

    Middleware updates

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,28 +2,34 @@ import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 
 export const middleware = async (req: NextRequest) => {
+  const pathname = req.nextUrl.pathname;
   // Main page doesn't currently have content so users are redirected to games page
-  if (req.nextUrl.pathname === "/") {
+  if (pathname === "/") {
     return NextResponse.redirect(new URL("/games", req.url));
   }
 
   const token = await getToken({ req });
 
-  if (!token && req.nextUrl.pathname.startsWith("/user")) {
-    return NextResponse.redirect(new URL("/login", req.url));
-  }
-  const userIdMatch = req.nextUrl.pathname.match(/^\/user\/\d+$/);
-
-  if (!token && userIdMatch) {
+  // Direct non logged users to login page if trying to access user page
+  if (!token && pathname.startsWith("/user")) {
     return NextResponse.redirect(new URL("/login", req.url));
   }
 
-  return;
+  // Direct logged users to /games
+  if (
+    token &&
+    (pathname.startsWith("/signup") || pathname.startsWith("/login"))
+  ) {
+    return NextResponse.redirect(new URL("/games", req.url));
+  }
 
-  console.log(req.nextUrl.pathname);
-
-  if (!token && req.nextUrl.pathname !== "/login") {
-    return NextResponse.redirect(new URL("/login", req.url));
+  // Don't allow users to navigate to other users page
+  if (
+    token &&
+    pathname.match("/user") &&
+    !pathname.endsWith(token.user.username)
+  ) {
+    return NextResponse.redirect(new URL(`${token.user.username}`, req.url));
   }
 };
 
