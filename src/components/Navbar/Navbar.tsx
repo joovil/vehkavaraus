a7f25d5ab16e742c850a4a9f70b2be857c503958
JoonasMV <JoonasMRViljanen@gmail.com>
2025-02-07 16:32:55 +0200

    Added button to access admin panel, added icon for admins

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,14 +1,31 @@
+import { auth } from "@/app/api/auth/[...nextauth]/auth";
 import Image from "next/image";
 import Link from "next/link";
 import ShowUsername from "./components/ShowUsername";
 
 export default async function Navbar() {
+  const token = await auth();
+  const isAdmin = token?.user.role === "admin";
+
+  const href = isAdmin
+    ? "/admin/admin-panel"
+    : token
+      ? `/user/${token?.user.username}`
+      : "/games";
+
+  const src = isAdmin
+    ? "/icons/admin_horizontal_pink.svg"
+    : "/icons/vehka_horizontal_pink.svg";
+
   return (
     <nav className="relative flex items-center justify-between py-4 sm:justify-normal md:static">
-      <Link href={"/games"} className="hidden sm:block">
+      <Link
+        href={href}
+        className="hidden sm:block"
+      >
         <div className="flex-center flex">
           <Image
-            src="/icons/vehka_horizontal_pink.svg"
+            src={src}
             height={0}
             width={0}
             alt="Pink vehka logo"
@@ -17,14 +34,16 @@ export default async function Navbar() {
         </div>
       </Link>
 
-      <Link href={"/games"} className="text-2xl font-bold">
+      <Link
+        href={"/games"}
+        className="text-2xl font-bold"
+      >
         <span className="block md:inline-block">Vehkapolku&apos;s</span>
         <span className="block italic md:inline-block">Rent-A-Game</span>
       </Link>
 
       <div className="flex flex-col text-right text-lg sm:ml-auto">
         <ShowUsername />
-        {/* <LogoutButton /> */}
       </div>
     </nav>
   );
