50063d0e8aa4f953d0180187fba431671382a617
JoonasMV <JoonasMRViljanen@gmail.com>
2025-04-02 09:18:24 +0300

    Remove unused files

--- src/lib/actions/borrows/getBorrowByGameIdAction.ts
+++
@@ -1,8 +0,0 @@
-"use server";
-
-import { getBorrowByGameId } from "@/database/repositories/borrowRepository";
-
-export const getBorrowByGameIdAction = async (id: number) => {
-  // TODO: Only admins should be able to access this data
-  return await getBorrowByGameId(id);
-};
--- src/lib/actions/games/getAllGamesAction.ts
+++
@@ -1,8 +0,0 @@
-"use server";
-
-import { getAllGames } from "@/database/repositories/gameRepository";
-import { Game } from "@/types";
-
-export const getAllGamesAction = async (): Promise<Game[]> => {
-  return await getAllGames();
-};
