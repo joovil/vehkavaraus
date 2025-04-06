c39c124fcda9ae44501c5376c6ff97448adba51f
JoonasMV <JoonasMRViljanen@gmail.com>
2025-04-06 08:41:24 +0300

    Refactor game adding backend

--- src/lib/services/games/createGameService.ts
+++ src/lib/services/games/createGameService.ts
@@ -1,14 +1,28 @@
 import apiFetch from "@/lib/utils/apiFetch";
+import { Game } from "@/types";
 
-export const createGameService = async (name: string, image: File) => {
-  const formData = new FormData();
-  formData.append("name", name);
-  formData.append("file", image);
+export const createGameService = async (
+  name: string,
+  image: File,
+): Promise<Game> => {
+  try {
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("file", image);
 
-  const res = await apiFetch("/admin/games", {
-    method: "POST",
-    body: formData,
-  });
+    const res = await apiFetch("/admin/games", {
+      method: "POST",
+      body: formData,
+    });
 
-  return res;
+    if (!res.ok) {
+      const errorData = await res.json();
+      throw new Error("Error creating new game", errorData.error);
+    }
+
+    return res.json();
+  } catch (error) {
+    console.error("Error creating new game:", error);
+    throw error;
+  }
 };
